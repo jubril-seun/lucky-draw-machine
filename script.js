99d3b11c80cb8433445420713173ebebd637e621
“Seun <225109273+jubril-seun@users.noreply.github.com>
2026-04-11 10:36:41 -0400

    Create html and javascript file, and write pseudocode.

---
+++ script.js
@@ -0,0 +1,13 @@
+/* PSEUDOCODE
+Player inputs a number between 1 - 100
+The computer returns a random number between 1 - 100
+The player's number is compared against the computer's random number
+The results are determined based on how close the player's number is to the computer's number
+    Exact match = "Jackpot! You win the grand prize!"
+    Within 5 = "So close! You win a consolation prize."
+    Within 20 = "Nice try! You get a free game token."
+    Outside 20 = "No luck this time. Better luck next draw!"
+The result should also display the player's drawn number after each round
+After a round, the player should be asked if they want to play again
+The game should track and show total number of rounds played at the end */
+
