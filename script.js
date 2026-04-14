ac05ac35fb94669bb2e7a986a28c4ffb3ce79872
“Seun <225109273+jubril-seun@users.noreply.github.com>
2026-04-13 22:52:49 -0400

    Finish luck draw machine exercise with completed javascript.

--- script.js
+++ script.js
@@ -11,3 +11,68 @@ The result should also display the player's drawn number after each round
 After a round, the player should be asked if they want to play again
 The game should track and show total number of rounds played at the end */
 
+function playerNumber(){
+   let playerSelection = Number(prompt("Pick a number between 1 and 100: ", "Enter here"));
+    if(playerSelection >= 1 && playerSelection <= 100){
+        return playerSelection;
+    } else {
+        alert("Guess is out of range. Please try again.")
+        return playerNumber();
+    }
+}
+
+function randomNumber(){
+    let playerSelection = Math.floor(Math.random() * 100) + 1;
+    return playerSelection;
+}
+
+// console.log(playerNumber());
+// console.log(randomNumber());
+
+function compareNumbers(playerNumber, randomNumber){
+    console.log("You chose: " + playerNumber + " and the random number is: " + randomNumber)
+    
+    if(playerNumber === randomNumber){
+        console.log("Jackpot! You win the grand prize!")
+    } else if (Math.abs(playerNumber - randomNumber) <= 5){
+        console.log("So close! You win a consolation prize.")
+    } else if (Math.abs(playerNumber - randomNumber) <= 20){
+        console.log("Nice try! You get a free game token.")
+    } else {
+        console.log("No luck this time. Better luck next draw!")
+    }
+}
+
+let roundNumber = 0;
+
+function playAgainQuestion(){
+    
+    let playerAnswer = prompt("Would you like to play another round?","Yes or No?")
+    if(playerAnswer.toLowerCase() === "yes"){
+        roundNumber++;
+        return playRound();
+    } else if (playerAnswer.toLowerCase() === "no"){
+        roundNumber++;
+    } else {
+        alert("Please type yes or no")
+        return playAgainQuestion();
+    }
+}
+
+function playRound(){
+    
+    compareNumbers(playerNumber(),randomNumber());
+    playAgainQuestion();
+
+}
+// console.log(compareNumbers(playerNumber(),randomNumber()));
+
+// console.log(playRound());
+
+
+function playGame(){ 
+    playRound();
+    console.log("You have played " + roundNumber + " rounds.");
+}
+
+playGame();
